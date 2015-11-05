894459c47ceafaf431e58d8f913c84d35573c2e6
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-05 10:54:21 +0100

    correction de l'erreur de la class CalculPanier

--- src/main/java/edu/univ/nantes/CalculPanier.java
+++ src/main/java/edu/univ/nantes/CalculPanier.java
@@ -9,11 +9,11 @@ public class CalculPanier {
 	public static Logger log = Logger.getLogger(CalculPanier.class);
 
 	public double calcul(double prix, int quantite) {
-		double resultat = prix + quantite;
+		double resultat = prix * quantite;
 		log.debug(prix + "*" + quantite + "=" + resultat);
 		return resultat;
 	}
-	
+
 	public double calcul(String prixTexte, String quantiteTexte) {
 		double prixUnitaire = Double.parseDouble(prixTexte);
 		int quantite = Integer.parseInt(quantiteTexte);
@@ -29,7 +29,7 @@ public class CalculPanier {
 		log.info("Quantité:" + args[1]);
 		CalculPanier calcul = new CalculPanier();
 		log.info("Prix total:" + calcul.calcul(args[0], args[1]));
-		
+
 	}
 
 }
